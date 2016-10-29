f9d417b5d6e9e3152c255cbf97362f524edf1c3b
yaoyg14 <yaoyg14@mails.tsinghua.edu.cn>
2016-10-29 11:17:54 +0200

    Tony-new-trick
    Tony new!! see project1tony.ipynb and toolbox(saved in out.csv)

--- toolbox.py
+++ toolbox.py
@@ -1,97 +1,74 @@
 import numpy as np
+from scipy.special import expit
+from helpers import *
 
-def compute_loss(y, tx, w):
-    """Calculate the loss.
+def gradient_descent(y, tx, initial_w, gamma, max_iters, loss_func, grad_func):
+    """Gradient descent algorithm."""
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        grad = grad_func(y, tx, w)
+        w -= gamma * grad
         
-        You can calculate the loss using mse or mae.
-        """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE / MAE
-    # ***************************************************
-    return 1/2/len(y) *(y-tx@ w).T @ (y-tx@ w)
+    return loss_func(y, tx, w), w
 
-def compute_gradient(y, tx, w):
-    """Compute the gradient."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute gradient and loss
-    # ***************************************************
-    
-    
-    N=len(y)
-    
-    #coefficient of gradient is -1/N,but coefficient of compute_loss is 1/(2N)
-    
-    gradient=-1/N * (tx.T)  @ (y-tx@ w)
-    return gradient
+def stochastic_gradient_descent(y, tx, initial_w, gamma, max_iters, batch_size, loss_func, grad_func):
+    """Stochastic gradient descent algorithm."""
+    w = initial_w
+
+    for mini_y, mini_tx in batch_iter(y, tx, batch_size, max_iters):
+        grad = grad_func(mini_y, mini_tx, w)
+        w -= gamma * grad
+            
+    return loss_func(y, tx, w), w
+
+def compute_mse(y, tx, w):
+    """Calculate the loss using mse"""
+    e = y - tx @ w
+    return e.T @ e * 0.5 / len(y)
+
+def compute_mse_gradient(y, tx, w):
+    """Compute the gradient of mse"""
+    return -tx.T @ (y - tx @ w) / len(y)
 
 def least_squares(y, tx):
     """calculate the least squares solution."""
-    weight=np.linalg.solve(tx.T @ tx, tx.T @ y)
-    return weight
+    tx_t = tx.T
+    return np.linalg.solve(tx_t @ tx, tx_t @ y)
 
-def compute_stoch_gradient(y, tx, w):
-    """Compute a stochastic gradient for batch data."""
-    
-    
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient computation.
-    # ***************************************************
-    
-    
-    stoch_gradient=compute_gradient(y,tx,w)
-    loss=compute_loss(y,tx,w)
-    return stoch_gradient,loss
+def least_squares_GD(y, tx, gamma, max_iters):
+    """Gradient descent algorithm with mse."""
+    return gradient_descent(y, tx, np.zeros(tx.shape[1]), gamma, max_iters, compute_mse, compute_mse_gradient)
 
-
-def least_squares_GD (y, tx, initial_w, gamma, max_iters):
-    """Gradient descent algorithm."""
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    w = initial_w
-    losses = []
-    for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: compute gradient and loss
-        # ***************************************************
-        
-        loss=compute_loss(y,tx,w)
-        gradient=compute_gradient(y,tx,w)
-        
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: update w by gradient
-        # ***************************************************
-        
-        w=w-gamma*gradient
-        
-        # store w and loss
-        ws.append(np.copy(w))
-        losses.append(loss)
-    return losses, ws
-
-def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
-    """Stochastic gradient descent algorithm."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient descent.
-    # ***************************************************
-    w=initial_w
-    ws=[initial_w]
-    losses=[]
-    for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
-            stoch_gradient,loss=compute_stoch_gradient(minibatch_y,minibatch_tx,w)
-        w=w-gamma*stoch_gradient;
-        ws.append(np.copy(w))
-        losses.append(loss)
-    return losses, ws
+def least_squares_SGD(y, tx, gamma, max_iters, batch_size):
+    """Stochastic gradient descent algorithm with mse."""
+    return stochastic_gradient_descent(y, tx, np.zeros(tx.shape[1]), gamma, max_iters, batch_size, compute_mse, compute_mse_gradient)
 
 def ridge_regression(y, tx, lambda_):
     """implement ridge regression."""
-    I = np.eye(len(tx.T))
-    weight=np.linalg.solve(tx.T @ tx + lambda_*I,tx.T@ y)
-    return weight
+    tx_t = tx.T
+    return np.linalg.solve(tx_t @ tx + lambda_ * np.eye(len(tx_t)), tx_t @ y)
+    
+def logistic_regression(y, tx, gamma, max_iters):
+    """Logistic regression"""
+    def loss(y, tx, w):
+        #txn_t_x_w = tx @ w
+        #return np.abs(np.sum(np.log(1 + np.exp(txn_t_x_w)) - y * txn_t_x_w)) / len(y)
+        return 1
+    
+    def grad(y, tx, w):
+        return (tx.T @ (expit(tx @ w) - y)-2*0.022*w) / len(y)
+    
+    return stochastic_gradient_descent(y, tx, np.zeros(tx.shape[1]), gamma, max_iters,1000, loss, grad)
+
+def reg_logistic_regression(y, tx, gamma, max_iters):
+    """Logistic Ridge regression"""
+    def loss(y, tx, w):
+        #txn_t_x_w = tx @ 
+        #return np.abs(np.sum(np.log(1 + np.exp(txn_t_x_w)) - y * txn_t_x_w)) / len(y)
+        return 1
+    
+    def grad(y, tx, w):
+        return (tx.T @ (expit(tx @ w) - y)-2*0.1*w) / len(y)   #lambda_=0.1
+    
+    return stochastic_gradient_descent(y, tx, np.zeros(tx.shape[1]), gamma, max_iters,1000, loss, grad)
